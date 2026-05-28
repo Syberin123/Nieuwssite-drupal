3fdc11556ab178b2e8e0ff7ad2352867749ee53a
Sem Dekker <137659@student.dcterra.nl>
2026-05-28 16:18:38 +0200

    Add custom theme 'my_theme' with initial configuration and styles
    - Created 'my_theme' with regions for header, menus, content, and footer.
    - Added YAML configuration files for various blocks including account menu, breadcrumbs, content, help, and main menu.
    - Implemented views for latest articles with multiple blocks.
    - Added CSS files for base elements, components, layout, and theme styles.
    - Included JavaScript file for theme behaviors.
    - Added logo SVG and theme info, libraries, and theme functions.
    - Updated core menu link overrides and theme settings.

---
+++ web/themes/custom/my_theme/js/my-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * My-theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.myTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
